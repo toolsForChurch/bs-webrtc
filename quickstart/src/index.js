cc6afb86c3a4ff9c2754716e3dcc6d467df33067
isaac agaba <akaizat1@gmail.com>
2017-10-02 19:22:59 +0300

    Stable Confrence

--- quickstart/src/index.js
+++ quickstart/src/index.js
@@ -8,10 +8,65 @@ var identity;
 var roomName;
 var trackMode='all';
 var mute = false;
+var showVideo=true;
 var randomName = require('./randomname');
-var conversationsClient;
-var activeConversation;
-var accessManager;
+
+let toogleAudio = function (room) {
+    if (!mute) {
+        room.localParticipant.audioTracks.forEach(function (track) {
+                track.disable();
+            }
+        )
+        mute = true;
+        $('#mute').text("Un Mute");
+    }
+    else {
+        room.localParticipant.audioTracks.forEach(function (track) {
+                track.enable();
+            }
+        )
+        mute = false;
+        $('#mute').text("Mute");
+    }
+};
+let toogleVideo = function (room) {
+    if (showVideo) {
+
+        room.localParticipant.videoTracks.forEach(function (track) {
+                track.disable();
+            }
+        )
+        showVideo = false;
+        $('#show-video').text("Share Video");
+    }
+    else {
+        room.localParticipant.videoTracks.forEach(function (track) {
+                track.enable();
+            }
+        )
+        showVideo = trackMode;
+        $('#show-video').text("Turn off Video");
+    }
+};
+let makeCall = function (data) {
+    roomName = "biblestudy";
+
+    log("Joining room '" + roomName + "'...");
+    var connectOptions = {
+        name: roomName,
+        logLevel: 'debug'
+    };
+
+    if (previewTracks) {
+        connectOptions.tracks = previewTracks;
+    }
+
+    // Join the Room with the token from the server and the
+    // LocalParticipant's Tracks.
+    Video.connect(data.token, connectOptions).then(roomJoined, function (error) {
+        log('Could not connect to Twilio: ' + error.message);
+    });
+};
 
 // Attach the Tracks to the DOM.
 function attachTracks(tracks, container) {
@@ -46,58 +101,62 @@ function detachParticipantTracks(participant) {
 window.addEventListener('beforeunload', leaveRoomIfJoined);
 
 // Obtain a token from the server in order to connect to the Room.
+
 $.getJSON('/webrtc/token?name='+randomName(), function(data) {
   identity = data.identity;
 
 
   // Bind button to join Room.
-  document.getElementById('button-join').onclick = function() {
-    roomName = "biblestudy";
+  document.getElementById('button-join-audio').onclick = function() {
+      makeCall(data);
+      showVideo=false;
+      $('#show-video').text("Share Video");
 
-    log("Joining room '" + roomName + "'...");
-    var connectOptions = {
-      name: roomName,
-      logLevel: 'debug'
-    };
 
-    if (previewTracks) {
-      connectOptions.tracks = previewTracks;
-    }
-
-    // Join the Room with the token from the server and the
-    // LocalParticipant's Tracks.
-    Video.connect(data.token, connectOptions).then(roomJoined, function(error) {
-      log('Could not connect to Twilio: ' + error.message);
-    });
   };
 
-  // Bind button to leave Room.
+    document.getElementById('button-join-video').onclick = function() {
+
+        makeCall(data);
+    };
+
+
+    // Bind button to leave Room.
   document.getElementById('button-leave').onclick = function() {
     log('Leaving room...');
     activeRoom.disconnect();
   };
-    // Bind button switch input.
-    document.getElementById('switch-input').onclick = function() {
 
-
-    };
 
 });
 
 // Successfully connected!
+
 function roomJoined(room) {
-  window.room = activeRoom = room;
+    window.room = activeRoom = room;
 
 
   log("Joined as '" + identity + "'");
-  document.getElementById('button-join').style.display = 'none';
-  document.getElementById('button-leave').style.display = 'inline';
+    document.getElementById('button-join-audio').style.display = 'none';
+    document.getElementById('button-join-video').style.display = 'none';
+    document.getElementById('button-leave').style.display = 'inline';
+    document.getElementById('show-video').style.display = 'inline';
+    document.getElementById('mute').style.display = 'inline';
 
   // Attach LocalParticipant's Tracks, if not already attached.
   var previewContainer = document.getElementById('local-media');
   if (!previewContainer.querySelector('video')) {
     attachParticipantTracks(room.localParticipant, previewContainer);
   }
+if(!showVideo){
+    room.localParticipant.videoTracks.forEach(function (track) {
+            track.disable();
+        }
+    )
+    showVideo = false;
+    $('#show-video').text("Share Video");
+}
+
 
   // Attach the Tracks of the Room's Participants.
   room.participants.forEach(function(participant) {
@@ -142,30 +201,17 @@ function roomJoined(room) {
     detachParticipantTracks(room.localParticipant);
     room.participants.forEach(detachParticipantTracks);
     activeRoom = null;
-    document.getElementById('button-join').style.display = 'inline';
+    document.getElementById('button-join-audio').style.display = 'inline';
+    document.getElementById('button-join-video').style.display = 'inline';
     document.getElementById('button-leave').style.display = 'none';
   });
     // Bind button to mute.
     document.getElementById('mute').onclick = function() {
-      if (!mute){
-        log("Mute")
-          room.localParticipant.audioTracks.forEach(function(track){
-                  track.disable();
-              }
-          )
-          mute=true;
-          log("Mic  mute")
-      }
-      else{
-          room.localParticipant.audioTracks.forEach(function(track){
-                  track.enable();
-              }
-          )
-          mute=false;
-          log("Mic un mute")
-      }
+      toogleAudio(room);
 
-
+    };
+    document.getElementById('show-video').onclick = function() {
+        toogleVideo(room);
 
     };
 }
@@ -191,9 +237,9 @@ document.getElementById('button-preview').onclick = function() {
 // Activity log.
 function log(message) {
 
-  var logDiv = document.getElementById('log');
-  logDiv.innerHTML += '<p>&gt;&nbsp;' + message + '</p>';
-  logDiv.scrollTop = logDiv.scrollHeight;
+  // var logDiv = document.getElementById('log');
+  // logDiv.innerHTML += '<p>&gt;&nbsp;' + message + '</p>';
+  // logDiv.scrollTop = logDiv.scrollHeight;
 }
 
 // Leave Room.
